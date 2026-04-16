25e28f9b80be7abbabc50712b5dc4f0f1c409305
Achintya088 <itagiachintya7@gmail.com>
2026-04-16 13:29:37 +0000

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,39 +2,85 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.WindowType;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class App {
-    public static void main(String[] args) {
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary("/usr/bin/firefox-esr");
-        options.addArguments("-private");
-       
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
 
-        WebDriver driver = new FirefoxDriver(options);
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+        
+
+        // Headless Firefox for Jenkins
+        FirefoxOptions options = new FirefoxOptions();
+
+options.setBinary("/usr/bin/firefox"); // force correct binary
+options.addArguments("--headless");
+
+//  VERY IMPORTANT for VM/Jenkins
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+
+// disable GPU (important sometimes)
+options.addArguments("--disable-gpu");
+
+WebDriver driver = new FirefoxDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        Actions actions = new Actions(driver);
 
         try {
-            driver.manage().window().maximize();
+            //  1. SauceDemo Login
+            driver.get("https://www.saucedemo.com/");
 
-            driver.get("https://www.firefox.com");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")))
+                    .sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
 
+            System.out.println("SauceDemo login successful ");
+
+            //  2. Automation Exercise (new tab)
             driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://automationexercise.com/products");
 
-            driver.get("https://www.amazon.in");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search_product")))
+                    .sendKeys("Men Tshirt");
+            driver.findElement(By.id("submit_search")).click();
 
-            wait.until(ExpectedConditions.elementToBeClickable(By.id("twotabsearchtextbox"))).sendKeys("shirt");
-            wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-search-submit-button"))).click();
+            WebElement product = wait.until(
+                    ExpectedConditions.elementToBeClickable(By.cssSelector("a[data-product-id='2']"))
+            );
 
+            product.click();
+
+            WebElement viewCart = wait.until(
+                    ExpectedConditions.elementToBeClickable(By.cssSelector("#cartModal a[href='/view_cart']"))
+            );
+            viewCart.click();
+
+            System.out.println("Automation Exercise product added to cart ");
+
+            // 3. Practice Test Automation (new tab)
+            driver.switchTo().newWindow(WindowType.TAB);
+            driver.get("https://practicetestautomation.com/practice-test-login/");
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")))
+                    .sendKeys("student");
+            driver.findElement(By.id("password")).sendKeys("Password123");
+            driver.findElement(By.id("submit")).click();
+
+            System.out.println("Practice Test Automation login successful ");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            driver.quit();
+            driver.quit(); 
         }
     }
 }
